bb3ee518edd25b250be97d91f6f8ed620ebde553
Jakub Jirák <jakub@jakubjirak.com>
2020-02-10 23:51:47 +0100

    Add error handeling in threads

--- src/main/java/com/jakubjirak/multithreading/examples/ThreadCreation.java
+++ src/main/java/com/jakubjirak/multithreading/examples/ThreadCreation.java
@@ -8,8 +8,18 @@ public class ThreadCreation {
                 //Code that will run in new thread
                 System.out.println("We are now in thread "+ Thread.currentThread().getName());
                 System.out.println("Current thread priority is "+ Thread.currentThread().getPriority());
+                throw new RuntimeException("Internal Exception");
             }
         });
+
+        thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler(){
+
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happended in thread " + t.getName() + " the error is " + e.getMessage());
+            }
+        });
+
         thread.setName("New worker thread");
         thread.setPriority(Thread.MAX_PRIORITY);
         System.out.println("We are in thread "+Thread.currentThread().getName()+" before starting a new thread.");
