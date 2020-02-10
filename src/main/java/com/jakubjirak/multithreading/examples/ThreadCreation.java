fa816145c8df3ce0bf0ce1d58e453ad7f6a17c37
Jakub Jirák <jakub@jakubjirak.com>
2020-02-10 23:47:13 +0100

    Add priority to worker thread

--- src/main/java/com/jakubjirak/multithreading/examples/ThreadCreation.java
+++ src/main/java/com/jakubjirak/multithreading/examples/ThreadCreation.java
@@ -7,9 +7,11 @@ public class ThreadCreation {
             public void run() {
                 //Code that will run in new thread
                 System.out.println("We are now in thread "+ Thread.currentThread().getName());
+                System.out.println("Current thread priority is "+ Thread.currentThread().getPriority());
             }
         });
         thread.setName("New worker thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
         System.out.println("We are in thread "+Thread.currentThread().getName()+" before starting a new thread.");
         thread.start();
         System.out.println("We are in thread "+Thread.currentThread().getName()+" after starting a new thread.");
